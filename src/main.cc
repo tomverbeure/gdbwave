ffae440926dd02fa53e555f5737a9c62f79233ca
Tom Verbeure <hombre+github.com@gmail.com>
2021-11-11 13:37:05 -0800

    Open FST file successfully.

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,22 @@
 
-#include <fstapi.h>
+#include <iostream>
+
+#include <fst/fstapi.h>
+
+using namespace std;
+
+char fstName[] = "../test_data/top.fst";
 
 int main(int argc, char **argv)
 {
+
+    void *fstCtx;
+
+    fstCtx = fstReaderOpen((const char *)fstName);
+    if (!fstCtx){
+        cerr << "Can't open " << fstName << endl;
+        exit(-1);
+    }
+
     return 0;
 }
